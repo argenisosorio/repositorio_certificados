ae0417669d4eba1daba8ceef14b0ff7948b620ce
aosorio <argenisosorio580@hotmail.com>
2018-04-04 17:09:54 -0400

    Arreglando el estilo visual y agregados los login required para las urls del admin

--- registro/urls.py
+++ registro/urls.py
@@ -3,14 +3,15 @@ from django.conf.urls import patterns, url
 from registro import views
 from registro.views import *
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = patterns('',
     #url(r'^$', Index.as_view(), name='index'),
-    url(r'^lista_certificados$', views.Lista_Certificados.as_view(), name='lista_certificados'),
-    url(r'^guardar_certificado$', views.Guardar_Certificado.as_view(), name='guardar_certificado'),
-    url(r'^editar_certificado/(?P<pk>\d+)$', views.Editar_Certificado.as_view(), name='editar_certificado'),
-    url(r'^borrar_certificado/(?P<pk>\d+)$', views.Borrar_Certificado.as_view(), name='borrar_certificado'),
+    url(r'^lista_certificados$', login_required(views.Lista_Certificados.as_view()), name='lista_certificados'),
+    url(r'^guardar_certificado$', login_required(views.Guardar_Certificado.as_view()), name='guardar_certificado'),
+    url(r'^editar_certificado/(?P<pk>\d+)$', login_required(views.Editar_Certificado.as_view()), name='editar_certificado'),
+    url(r'^borrar_certificado/(?P<pk>\d+)$', login_required(views.Borrar_Certificado.as_view()), name='borrar_certificado'),
     # Definiendo la url que va a servir los certificados para que puedan ser descargados.
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
         'document_root': settings.MEDIA_ROOT,
