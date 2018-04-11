35de286c5ad84f1cd65c11d04e34e5ec7390a99a
aosorio <argenisosorio580@hotmail.com>
2018-04-11 13:16:53 -0400

    Agregada la funcion para desabilitar el copy and paste y el cursor

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,27 @@
 $("document").ready(function(){
 
+    /*
+  |------------------------------------------
+  | Función que desabilita el copy and paste.
+  |------------------------------------------
+  */
+   $('#txtInput1').on("cut copy paste",function(e) {
+     e.preventDefault();
+   });
+
+  /*
+  |--------------------------------------------
+  | Función que desabilita el campo del código.
+  |--------------------------------------------
+  */
+  $("document").ready(function () {
+    //$("#txtCaptcha").prop('disabled', true);
+    //$('#txtCaptchaDiv').addClass('disabled');
+    //$("span").unbind("click");
+    //$("span").attr("disabled", true);
+    //$("span").css("pointer-events", "none"); // Desabilitar el puntero para selección de texto
+  });
+
   /*
   |--------------------------------------------------------------------------
   | Función que hace desaparecer las notificaciones(alerts) de bootstrap.
