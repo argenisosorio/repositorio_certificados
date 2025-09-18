a40f683844f55ecdedd009ff3f5002637425b198
Argenis Osorio <argenisosorio580@hotmail.com>
2025-09-18 19:57:55 -0400

    feat(Urls): Agregada url para devolver la versión del software

--- repositorio_certificados/urls.py
+++ repositorio_certificados/urls.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
+import os
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import JsonResponse
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('registro.urls', namespace='registro')),
+    # Devolver la versión del software.
+    url(r'^version/$',
+        lambda request: JsonResponse({
+            'version': open(os.path.join(settings.BASE_DIR, 'VERSION.txt')).read().strip()
+        }),
+        name='version'),
 ]
